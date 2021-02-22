1e856e7ce5f58833adab9d18372bd5a0f3b822f2
pauga <pau.galan22@gmail.com>
2021-02-22 17:43:14 +0100

    ssprartaf

--- src/main/java/upf/edu/TwitterLanguageFilterApp.java
+++ src/main/java/upf/edu/TwitterLanguageFilterApp.java
@@ -6,8 +6,20 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
 import scala.Tuple2;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class TwitterLanguageFilterApp {
     public static void main(String[] args){
-        System.out.println("Hola Mundo");
+        String input = args[0];
+        String outputDir = args[1];
+
+        //Create a SparkContext to initialize
+        SparkConf conf = new SparkConf().setAppName("Twitter Filter");
+        JavaSparkContext sparkContext = new JavaSparkContext(conf);
+        // Load input
+        JavaRDD<String> sentences = sparkContext.textFile(input);
     }
 }
