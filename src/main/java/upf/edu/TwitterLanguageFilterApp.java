4ebc1a9d1d2f962456517237e18ef7f385d2c77e
Eva Martos Solano <eva.martos.solano@avanade.com>
2021-02-23 20:27:09 +0100

    Comentario eva + cogida argumentos

--- src/main/java/upf/edu/TwitterLanguageFilterApp.java
+++ src/main/java/upf/edu/TwitterLanguageFilterApp.java
@@ -14,8 +14,10 @@ import java.util.Arrays;
 
 public class TwitterLanguageFilterApp {
     public static void main(String[] args){
-        String input = args[0];
-        String outputDir = args[1];
+        List<String> argsList = Arrays.asList(args);
+        String lang = argsList.get(0);
+        String outputDir = argsList.get(1);
+        String input= argsList.get(2);
 
         //Create a SparkContext to initialize
         SparkConf conf = new SparkConf().setAppName("Twitter Filter");
@@ -28,5 +30,12 @@ public class TwitterLanguageFilterApp {
                 .reduceByKey((a, b) -> a + b);
         System.out.println("Total words: " + counts.count());
         counts.saveAsTextFile(outputDir);
+
+        /*JavaRDD<String> sentences = sparkContext.textFile(input).filter(x -> !x.isEmpty());
+		JavaRDD<String> tweets = sentences.map(word -> SimplifiedTweet.fromJson(word)).filter(s-> s.isPresent()) // filter para tener solo los que tienen algo del optional
+				.map(s -> s.get()) // pasa de optional a SimplifiedTweet
+				.filter(s -> s.getLanguage().equals(lang)) // filtrar idioma
+				.map(s -> s.toString()); // este nos pasa de SimplifiedTweet a String
+		tweets.saveAsTextFile(outputDir);*/
     }
 }
