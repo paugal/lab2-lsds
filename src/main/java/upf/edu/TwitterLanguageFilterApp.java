6679865475b0b1252477ab8781e24ab090b13533
Alex Pujol Comet <alexpujolcomet8@gmail.com>
2021-02-27 17:06:37 +0100

    time processing

--- src/main/java/upf/edu/TwitterLanguageFilterApp.java
+++ src/main/java/upf/edu/TwitterLanguageFilterApp.java
@@ -14,19 +14,25 @@ public class TwitterLanguageFilterApp {
         List<String> argsList = Arrays.asList(args);
         String lang = argsList.get(0);
         String outputDir = argsList.get(1);
-        String input= argsList.get(2);
+        String input = argsList.get(2);
+
+        long start = System.currentTimeMillis();
 
         //Create a SparkContext to initialize
         SparkConf conf = new SparkConf().setAppName("Twitter Filter");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
         JavaRDD<String> sentences = sparkContext.textFile(input)
-                .filter(sentence -> !sentence.isEmpty())
-                .map(word -> SimplifiedTweet.fromJson(word)) //Get Optional from tweet json
-                .filter(s-> s.isPresent()) // Filter only present
-				.map(s -> s.get()) // Optional to SimplifiedTweet
-				.filter(s -> s.getLanguage().equals(lang)) // Filter language
-				.map(s -> s.toString()); // SimplifiedTweet to String
+            .filter(sentence -> !sentence.isEmpty())
+            .map(word -> SimplifiedTweet.fromJson(word)) // Get Optional from tweet json
+            .filter(s-> s.isPresent()) // Filter only present
+            .map(s -> s.get()) // Optional to SimplifiedTweet
+            .filter(s -> s.getLanguage().equals(lang)) // Filter language
+            .map(s -> s.toString()); // SimplifiedTweet to String
 		sentences.saveAsTextFile(outputDir);
+
+        long end = System.currentTimeMillis();
+        long elapsedTime = end - start;
+        System.out.println("Time to process: " + elapsedTime);
     }
 }
